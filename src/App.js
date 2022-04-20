52268c1c5daf4907787510a5a0ef745408d5363b
Spring <bartekkuleta21@gmail.com>
2022-04-20 20:56:19 +0200

    add basic structure with sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React, { useState } from 'react'
+import './css/style.css'
+import Menu from './Menu'
+import Content from './Content'
+
 function App() {
-  return <div></div>
+  return (
+    <div className="wrapper">
+      <nav>
+        <Menu />
+      </nav>
+      <section>{/* <Content /> */}</section>
+    </div>
+  )
 }
 
 export default App
